e5c1e8bf38c71993497bc40d99218328859e601d
unknown <uniholder@gmail.com>
2021-06-30 02:40:07 +0300

    hide invisible parts of garment

--- 14_3D_ML_project/Project/p3d_renderer.py
+++ 14_3D_ML_project/Project/p3d_renderer.py
@@ -99,7 +99,6 @@ class Pytorch3dRenderer(object):
             )
         self.renderer_medium = self.__get_renderer(self.render_size_medium, lights)
 
-
         # renderer for small objects, such as whole body.
         self.render_size_small = 200
         if ambient == False:
@@ -109,11 +108,10 @@ class Pytorch3dRenderer(object):
                 device=self.device, location=[[1.0, 1.0, -30]])
         else:
             lights = AmbientLights(
-                ambient_color = [[0.5, 0.5, 0.5],],
+                ambient_color = [[0.5, 0.5, 0.5], ],
                 device=self.device
             )
         self.renderer_small = self.__get_renderer(self.render_size_small, lights)
-
 
     def __get_renderer(self, render_size, lights):
 
@@ -149,7 +147,6 @@ class Pytorch3dRenderer(object):
         )
 
         return renderer
-    
 
     def render(self, verts_body, faces_body, verts_garment, faces_garment, bg_img, texture_rgb=None):
         verts_body = verts_body.copy()
@@ -158,28 +155,20 @@ class Pytorch3dRenderer(object):
         faces_garment = faces_garment.copy()
 
         # bbox for verts
-        x0_b = int(np.min(verts_body[:, 0]))
-        x1_b = int(np.max(verts_body[:, 0]))
-        y0_b = int(np.min(verts_body[:, 1]))
-        y1_b = int(np.max(verts_body[:, 1]))
-        width_b = x1_b - x0_b
-        height_b = y1_b - y0_b
-        
-        x0_g = int(np.min(verts_garment[:, 0]))
-        x1_g = int(np.max(verts_garment[:, 0]))
-        y0_g = int(np.min(verts_garment[:, 1]))
-        y1_g = int(np.max(verts_garment[:, 1]))
-        width_g = x1_g - x0_g
-        height_g = y1_g - y0_g
+        x0 = int(np.min(verts_body[:, 0]))
+        x1 = int(np.max(verts_body[:, 0]))
+        y0 = int(np.min(verts_body[:, 1]))
+        y1 = int(np.max(verts_body[:, 1]))
+        width = x1 - x0
+        height = y1 - y0
 
-        bbox_size_b = max(height_b, width_b)
-        bbox_size_g = max(height_g, width_g)
-        if bbox_size_b <= self.render_size_small:
+        bbox_size = max(height, width)
+        if bbox_size <= self.render_size_small:
             print("Using small size renderer")
             render_size = self.render_size_small
             renderer = self.renderer_small
         else:
-            if bbox_size_b <= self.render_size_medium:
+            if bbox_size <= self.render_size_medium:
                 print("Using medium size renderer")
                 render_size = self.render_size_medium
                 renderer = self.renderer_medium
@@ -189,35 +178,27 @@ class Pytorch3dRenderer(object):
                 renderer = self.renderer_large
         
         # padding the tight bbox
-        margin_b = int(max(width_b, height_b) * 0.1)
-        x0_b = max(0, x0_b-margin_b)
-        y0_b = max(0, y0_b-margin_b)
-        x1_b = min(self.img_size, x1_b+margin_b)
-        y1_b = min(self.img_size, y1_b+margin_b)
-        
-        margin_g = int(max(width_g, height_g) * 0.1)
-        x0_g = max(0, x0_g-margin_g)
-        y0_g = max(0, y0_g-margin_g)
-        x1_g = min(self.img_size, x1_g+margin_g)
-        y1_g = min(self.img_size, y1_g+margin_g)
+        margin = int(max(width, height) * 0.1)
+        x0 = max(0, x0-margin)
+        y0 = max(0, y0-margin)
+        x1 = min(self.img_size, x1+margin)
+        y1 = min(self.img_size, y1+margin)
 
         # move verts to be in the bbox
-        verts_body[:, 0] -= x0_b
-        verts_body[:, 1] -= y0_b
+        verts_body[:, 0] -= x0
+        verts_body[:, 1] -= y0
         
-        verts_garment[:, 0] -= x0_g
-        verts_garment[:, 1] -= y0_g
+        verts_garment[:, 0] -= x0
+        verts_garment[:, 1] -= y0
 
         # normalize verts to (-1, 1)
-        bbox_size_b = max(y_b1-y0_b, x1_b-x0_b)
-        half_size_b = bbox_size_b / 2
-        verts_body[:, 0] = (verts_body[:, 0] - half_size_b) / half_size_b
-        verts_body[:, 1] = (verts_body[:, 1] - half_size_b) / half_size_b
+        bbox_size = max(y1-y0, x1-x0)
+        half_size = bbox_size / 2
+        verts_body[:, 0] = (verts_body[:, 0] - half_size) / half_size
+        verts_body[:, 1] = (verts_body[:, 1] - half_size) / half_size
         
-        bbox_size_g = max(y1_g-y0_g, x1_g-x0_g)
-        half_size_g = bbox_size_g / 2
-        verts_garment[:, 0] = (verts_garment[:, 0] - half_size_g) / half_size_g
-        verts_garment[:, 1] = (verts_garment[:, 1] - half_size) / half_size_g
+        verts_garment[:, 0] = (verts_garment[:, 0] - half_size) / half_size
+        verts_garment[:, 1] = (verts_garment[:, 1] - half_size) / half_size
 
         # the coords of pytorch-3d is (1, 1) for upper-left and (-1, -1) for lower-right
         # so need to multiple minus for vertices
@@ -245,80 +226,31 @@ class Pytorch3dRenderer(object):
         # rendering
         mesh_body = Meshes(verts=verts_tensor_b, faces=faces_tensor_b, textures=textures_b)
         mesh_garment = Meshes(verts=verts_tensor_g, faces=faces_tensor_g, textures=textures_g)
-        
-        # blending rendered mesh with background image
-        rend_img = renderer(mesh_body)
-        rend_img = rend_img[0].cpu().numpy()
 
-
-        scale_ratio = render_size / bbox_size_b
-        img_size_new = int(self.img_size * scale_ratio)
-        bg_img_new = cv2.resize(bg_img, (img_size_new, img_size_new))
-
-        x0 = max(int(x0_b * scale_ratio), 0)
-        y0 = max(int(y0_b * scale_ratio), 0)
-        x1 = min(int(x1_b * scale_ratio), img_size_new)
-        y1 = min(int(y1_b * scale_ratio), img_size_new)
-
-        h0 = min(y1_b-y0_b, render_size)
-        w0 = min(x1_b-x0_b, render_size)
-
-        y1 = y0 + h0
-        x1 = x0 + w0
-
-        rend_img_new = np.zeros((img_size_new, img_size_new, 4))
-        rend_img_new[y0:y1, x0:x1, :] = rend_img[:h0, :w0, :]
-        rend_img = rend_img_new
-
-        alpha = rend_img[:, :, 3:4]
-        alpha[alpha>0] = 1.0
-        
-
-        rend_img = rend_img[:, :, :3] 
-        maxColor = rend_img.max()
-        rend_img *= 255 /maxColor #Make sure <1.0
-        rend_img = rend_img[:, :, ::-1]
-
-        res_img = alpha * rend_img + (1.0 - alpha) * bg_img_new
-
-        res_img = cv2.resize(res_img, (self.img_size, self.img_size))
-
-        return res_img
-        
-
-        # bbox for verts
-        x0 = int(np.min(verts_body[:, 0]))
-        x1 = int(np.max(verts_body[:, 0]))
-        y0 = int(np.min(verts_body[:, 1]))
-        y1 = int(np.max(verts_body[:, 1]))
-        width = x1 - x0
-        height = y1 - y0
-
-        bbox_size = max(height, width)
-        
-        mesh_body = self.get_mesh(verts_body, faces_body)
-        mesh_garment = self.get_mesh(verts_garment, faces_garment, texture_rgb)
-
+        # create z-mask
         fragments_body = renderer.rasterizer(mesh_body)
         fragments_garment = renderer.rasterizer(mesh_garment)
-        
+
         z_body = fragments_body.zbuf.squeeze(3).squeeze(0)
         z_garment = fragments_garment.zbuf.squeeze(3).squeeze(0)
-        
-        mask = z_garment > z_body
-        
-        print(fragments_garment.zbuf.shape)
-        
+
+        eps = 0.1
+        mask = (z_garment < z_body+eps).cpu().numpy()
+        mask = np.repeat(mask[:, :, np.newaxis], 4, axis=2)
+        # print('mask:', mask.shape)
         
         # blending rendered mesh with background image
         rend_img = renderer(mesh_garment)
+        # print('rend_img:', rend_img.shape)
         rend_img = rend_img[0].cpu().numpy()
-        print(rend_img.shape)
 
+        rend_img = rend_img * mask
 
         scale_ratio = render_size / bbox_size
         img_size_new = int(self.img_size * scale_ratio)
+        # print('bg_img:', bg_img.shape)
         bg_img_new = cv2.resize(bg_img, (img_size_new, img_size_new))
+        # print('bg_img_new:', bg_img_new.shape)
 
         x0 = max(int(x0 * scale_ratio), 0)
         y0 = max(int(y0 * scale_ratio), 0)
@@ -337,16 +269,15 @@ class Pytorch3dRenderer(object):
 
         alpha = rend_img[:, :, 3:4]
         alpha[alpha>0] = 1.0
-        
 
-        rend_img = rend_img[:, :, :3] 
+
+        rend_img = rend_img[:, :, :3]
         maxColor = rend_img.max()
         rend_img *= 255 /maxColor #Make sure <1.0
         rend_img = rend_img[:, :, ::-1]
 
         res_img = alpha * rend_img + (1.0 - alpha) * bg_img_new
-
+        # print('res_img:', res_img.shape)
         res_img = cv2.resize(res_img, (self.img_size, self.img_size))
-        print(res_img.shape)
 
         return res_img
